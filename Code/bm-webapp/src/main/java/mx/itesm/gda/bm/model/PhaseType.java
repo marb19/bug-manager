543f0ac223236727b9501ef09456ea1766040a26
lalo.campos@gmail.com <lalo.campos@gmail.com@b2ca1986-ad76-bf60-f091-2a5095092597>
2011-12-14 16:20:44 +0000

    Agregando los enums de PhaseType.java y TaskType.java

---
+++ Code/bm-webapp/src/main/java/mx/itesm/gda/bm/model/PhaseType.java
@@ -0,0 +1,84 @@
+/* ***************************************************************************
+ *   Copyright (c) 2011 BM
+ *   Confidential and Proprietary
+ *   All Rights Reserved
+ *
+ * @(#)$Id: ReviewType.java 279 2010-10-30 15:43:29Z lalo.campos@gmail.com $
+ * Last Revised By   : $Author: lalo.campos@gmail.com $
+ * Last Checked In   : $Date: 2010-10-30 10:43:29 -0500 (Sat, 30 Oct 2010) $
+ * Last Version      : $Revision: 0 $
+ *
+ * Original Author   : Eduardo Campos
+ * Notes             :
+ * ************************************************************************ */
+
+package mx.itesm.gda.bm.model;
+
+/**
+ *
+ * @author $Author: lalo.campos@gmail.com $
+ * @version $Revision: 0 $
+ */
+
+public enum PhaseType {
+
+    REQUIREMENTS(true, false, false, false, false),
+    DESIGN(false, true, false, false, false),
+    CODING(false, false, true, false, false),
+    TESTING(false, false, false, true, false),
+    MAINTENANCE(false, false, false, false, true);
+
+    private final boolean requirements;
+
+    private final boolean design;
+
+    private final boolean coding;
+
+    private final boolean testing;
+
+    private final boolean maintenance;
+
+    PhaseType(boolean my_requirements, boolean my_design, boolean my_coding,
+            boolean my_testing, boolean my_maintenance){
+        requirements = my_requirements;
+        design = my_design;
+        coding = my_coding;
+        testing = my_testing;
+        maintenance = my_maintenance;
+    }
+
+    /**
+     * @return the requirements phase
+     */
+    public boolean isRequirements(){
+        return requirements;
+    }
+
+    /**
+     * @return the design phase
+     */
+    public boolean isDesign(){
+        return design;
+    }
+
+    /**
+     * @return the coding phase
+     */
+    public boolean isCoding(){
+        return coding;
+    }
+
+    /**
+     * @return the testing phase
+     */
+    public boolean isTesting(){
+        return testing;
+    }
+
+    /**
+     * @return the maintenance phase
+     */
+    public boolean isMaintenance(){
+        return maintenance;
+    }
+}
---
+++ Code/bm-webapp/src/main/java/mx/itesm/gda/bm/model/TaskType.java
@@ -0,0 +1,96 @@
+/* ***************************************************************************
+ *   Copyright (c) 2011 BM
+ *   Confidential and Proprietary
+ *   All Rights Reserved
+ *
+ * @(#)$Id: ReviewType.java 279 2010-10-30 15:43:29Z lalo.campos@gmail.com $
+ * Last Revised By   : $Author: lalo.campos@gmail.com $
+ * Last Checked In   : $Date: 2010-10-30 10:43:29 -0500 (Sat, 30 Oct 2010) $
+ * Last Version      : $Revision: 0 $
+ *
+ * Original Author   : Eduardo Campos
+ * Notes             :
+ * ************************************************************************ */
+
+package mx.itesm.gda.bm.model;
+
+/**
+ *
+ * @author $Author: lalo.campos@gmail.com $
+ * @version $Revision: 0 $
+ */
+
+public enum TaskType {
+
+    DEVELOPMENT(true, false, false, false, false, false),
+    PERSONAL_REVIEW(false, true, false, false, false, false),
+    PEER_REVIEW(false, false, true, false, false, false),
+    WALKTHROUGH(false, false, false, true, false, false),
+    INSPECTION(false, false, false, false, true, false),
+    TESTING(false, false, false, false, false, true);
+
+    private final boolean development;
+
+    private final boolean personalReview;
+
+    private final boolean peerReview;
+
+    private final boolean walk;
+
+    private final boolean inspection;
+
+    private final boolean testing;
+
+    TaskType(boolean my_development, boolean my_personalReview, boolean my_peerReview,
+            boolean my_walk, boolean my_inspection, boolean my_testing){
+        development = my_development;
+        personalReview = my_personalReview;
+        peerReview = my_peerReview;
+        walk = my_walk;
+        inspection = my_inspection;
+        testing = my_testing;
+    }
+
+    /**
+     * @return the development task
+     */
+     public boolean isDevelopment(){
+         return development;
+     }
+
+     /**
+     * @return the personal review task
+     */
+     public boolean isPersonalReview(){
+         return personalReview;
+     }
+
+     /**
+     * @return the peer review task
+     */
+     public boolean isPeerReview(){
+         return peerReview;
+     }
+
+     /**
+     * @return the walk task
+     */
+     public boolean isWalk(){
+        return walk;
+     }
+
+     /**
+     * @return the inspection task
+     */
+     public boolean isInspection(){
+         return inspection;
+     }
+
+     /**
+     * @return the testing task
+     */
+     public boolean isTesting(){
+         return testing;
+     }
+}
+
